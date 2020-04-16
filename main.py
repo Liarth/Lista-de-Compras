430cbc34328bc539d4fe49e875b8fba27f6d25b5
Liarth <davitostes39@gmail.com>
2020-04-16 13:36:23 +0000

    Verificador de item(funcionando)

--- main.py
+++ main.py
@@ -29,19 +29,16 @@ Digite uma das opções acima: '''))
         if char.nome == nwProduto.nome:
           print("--------------------------")
           print("-Item com mesmo nome encontrado-")
-          rm = str(input("Deseja removê-lo? y/n >"))
-          if rm == "y":
-            del lista[id]
-            print("-Item removido e substituido-")
-            lista.append(nwProduto)
-            break
-          if rm == "n":
-            print("-Ok, item antigo mantido-")
-            break
+          break
+        else:
+          lista.append(nwProduto)
+          print("--------------------------")
+          print("-Item adicionado-")
+          break
     else:
       lista.append(nwProduto)
       print("--------------------------")
-      print("-Item removido-")
+      print("-Item adicionado-")
 
   if option == 2:
     rmProduto = str(input("Digite o nome do item que deseja remover: "))
@@ -61,9 +58,6 @@ Digite uma das opções acima: '''))
       print("Nome: {}".format(char.nome))
       print("Valor: {}".format(char.valor))
       print("Marca: {}".format(char.marca))
-    if len(lista) == 0:
-      print("--------------------------")
-      print("-Não há nenhum item na lista-")
 
   if option == 4:
     print("Finalizando programa...")
